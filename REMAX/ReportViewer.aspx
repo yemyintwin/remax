846bc8fa05f2f053bd367e048d07820103d8e948
yemyintwin <yemyintwin@gmail.com>
2019-03-28 22:13:23 +0800

    Report Viewer Added

---
+++ REMAX/ReportViewer.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ReportViewer.aspx.cs" Inherits="_Default" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+    
+    </div>
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server">
+        </rsweb:ReportViewer>
+    </form>
+</body>
+</html>
---
+++ REMAX/ReportViewer.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Net;
+using Microsoft.Reporting.WebForms;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            ReportViewer1.Width = 800;
+            ReportViewer1.Height = 600;
+            ReportViewer1.ProcessingMode = ProcessingMode.Remote;
+            IReportServerCredentials irsc = new CustomReportCredentials("hiroodaikai-001", "yanmarsucks66!", "ifc"); // e.g.: ("demo-001", "123456789", "ifc")
+            ReportViewer1.ServerReport.ReportServerCredentials = irsc;
+            ReportViewer1.ServerReport.ReportServerUrl = new Uri("http://sql5030.site4now.net/ReportServer");
+            ReportViewer1.ServerReport.ReportPath = "/hiroodaikai-001/TestReport"; //e.g.: /demo-001/test
+            ReportViewer1.ServerReport.Refresh();
+        }
+    }
+}
+
+public class CustomReportCredentials : IReportServerCredentials
+{
+    private string _UserName;
+    private string _PassWord;
+    private string _DomainName;
+
+    public CustomReportCredentials(string UserName, string PassWord, string DomainName)
+    {
+        _UserName = UserName;
+        _PassWord = PassWord;
+        _DomainName = DomainName;
+    }
+
+    public System.Security.Principal.WindowsIdentity ImpersonationUser
+    {
+        get { return null; }
+    }
+
+    public ICredentials NetworkCredentials
+    {
+        get { return new NetworkCredential(_UserName, _PassWord, _DomainName); }
+    }
+
+    public bool GetFormsCredentials(out Cookie authCookie, out string user,
+     out string password, out string authority)
+    {
+        authCookie = null;
+        user = password = authority = null;
+        return false;
+    }
+}
